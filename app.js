d5deb4e898c8ed2f3e191f949b8946a7fafe5703
Harin-Mabao <mixharinmabao@gmail.com>
2024-06-24 01:17:02 +0700

    Update app.js Rev00 First issued

--- app.js
+++ app.js
@@ -1,1 +1,5 @@
-// write code here
+let BRAND_NAME = 'I am a hero'
+let BIG_BRAND_NAME = BRAND_NAME.toUpperCase();
+
+console.log( ` Original = ${BRAND_NAME} \n Modified =${BIG_BRAND_NAME}`);
+alert( ` Original = ${BRAND_NAME} \n Modified =${BIG_BRAND_NAME}`);
